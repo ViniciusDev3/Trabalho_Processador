9b38a29da8d39116f1acee3a0a365bee9236b9c5
ViniciusDev3 <vinicius.g.dev@outlook.com>
2025-07-14 02:26:09 -0300

    Update BNEG & BZERO

--- processador.cpp
+++ processador.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -6,6 +7,7 @@
 
 using namespace std;
 
+int ultimoValor = 0;
 void moveRegis(string regist1, string regist2)
 {
     //Vai ler todos os dados do arquivo e colocar em um vetor
@@ -48,6 +50,7 @@ void moveRegis(string regist1, string regist2)
             linhas[i] = regist1 + ": " + valor;
         }
     }
+    ultimoValor = stoi(valor);
 
     //Registra nos registradores R0, R1, R3, R4 nos novos valores
     ofstream regist("banco_registradores.txt");
@@ -60,6 +63,7 @@ void moveRegis(string regist1, string regist2)
     } else {
         cout << "Erro ao abrir o arquivo!!!" << endl;
     }
+    
 }
 
 void registradores(string registrador, string M) 
@@ -107,6 +111,7 @@ void registradores(string registrador, string M)
             }
         }
         linhas[i] = registrador + ": " + valorM;
+        ultimoValor = stoi(valorM);
         memoriaR.close();
     } else {
         cout << "Erro ao abrir o arquivo de entrada" << endl;
@@ -146,14 +151,187 @@ string lerValorRegistrador(const string& registrador)
     return "";
 }
 
-int contPC = 0;
-void unicadeC(string instr, string valor1, string valor2, string valor3) 
+void add(string destino, string origem1, string origem2)
+{
+    string v1 = lerValorRegistrador(origem1);
+    string v2 = lerValorRegistrador(origem2);
+
+    int val1 = stoi(v1);
+    int val2 = stoi(v2);
+
+    int soma = val1 + val2;
+    ultimoValor = soma; 
+    ifstream registR("banco_registradores.txt");
+    vector <string> linhas;
+    string linha;
+
+    while(getline(registR, linha))
+    {
+        linhas.push_back(linha);
+    }
+
+    registR.close();
+
+    for(size_t i = 0; i < linhas.size(); i++)
+    {
+        istringstream iss(linhas[i]);
+        string reg;
+        getline(iss, reg, ':');
+
+        if (reg == destino) 
+        {
+            linhas[i] = destino + ": " + to_string(soma);
+        }
+    }
+
+    ofstream registW("banco_registradores.txt");
+    for (const auto& l : linhas) 
+    {
+        registW << l << endl;
+    }
+    registW.close();
+    
+    cout << "ADD" << endl;    
+}
+
+void sub(string destino, string origem1, string origem2)
+{
+    string v1 = lerValorRegistrador(origem1);
+    string v2 = lerValorRegistrador(origem2);
+
+    int val1 = stoi(v1);
+    int val2 = stoi(v2);
+
+    int subtracao = val1 - val2;
+    ultimoValor = subtracao; 
+
+    ifstream registR("banco_registradores.txt");
+    vector <string> linhas;
+    string linha;
+
+    while(getline(registR, linha))
+    {
+        linhas.push_back(linha);
+    }
+
+    registR.close();
+
+    for(size_t i = 0; i < linhas.size(); i++)
+    {
+        istringstream iss(linhas[i]);
+        string reg;
+        getline(iss, reg, ':');
+
+        if (reg == destino) 
+        {
+            linhas[i] = destino + ": " + to_string(subtracao);
+        }
+    }
+
+    ofstream registW("banco_registradores.txt");
+    for (const auto& l : linhas) 
+    {
+        registW << l << endl;
+    }
+    registW.close();
+
+    cout << "SUB" << endl;    
+}
+
+void orReg(string destino, string origem1, string origem2)
+{
+    string v1 = lerValorRegistrador(origem1);
+    string v2 = lerValorRegistrador(origem2);
+
+    int val1 = stoi(v1);
+    int val2 = stoi(v2);
+
+    int resultado = val1 | val2;
+    ultimoValor = resultado; 
+
+    ifstream registR("banco_registradores.txt");
+    vector <string> linhas;
+    string linha;
+
+    while(getline(registR, linha))
+    {
+        linhas.push_back(linha);
+    }
+
+    registR.close();
+
+    for(size_t i = 0; i < linhas.size(); i++)
+    {
+        istringstream iss(linhas[i]);
+        string reg;
+        getline(iss, reg, ':');
+
+        if (reg == destino) 
+        {
+            linhas[i] = destino + ": " + to_string(resultado);
+        }
+    }
+
+    ofstream registW("banco_registradores.txt");
+    for (const auto& l : linhas) 
+    {
+        registW << l << endl;
+    }
+    registW.close();
+
+    cout << "OR" << endl;    
+}
+
+void andReg(string destino, string origem1, string origem2)
+{
+    string v1 = lerValorRegistrador(origem1);
+    string v2 = lerValorRegistrador(origem2);
+
+    int val1 = stoi(v1);
+    int val2 = stoi(v2);
+
+    int resultado = val1 & val2;
+    ultimoValor = resultado; 
+
+    ifstream registR("banco_registradores.txt");
+    vector <string> linhas;
+    string linha;
+
+    while(getline(registR, linha))
+    {
+        linhas.push_back(linha);
+    }
+
+    registR.close();
+
+    for(size_t i = 0; i < linhas.size(); i++)
+    {
+        istringstream iss(linhas[i]);
+        string reg;
+        getline(iss, reg, ':');
+
+        if (reg == destino) 
+        {
+            linhas[i] = destino + ": " + to_string(resultado);
+        }
+    }
+
+    ofstream registW("banco_registradores.txt");
+    for (const auto& l : linhas) 
+    {
+        registW << l << endl;
+    }
+    registW.close();
+
+    cout << "AND" << endl;    
+}
+
+void unicadeC(int PC, string instr, string valor1, string valor2, string valor3) 
 {
     ofstream unicadeControle("unicade_controle.txt", ios::app);
     if (unicadeControle.is_open())
     {
-        unicadeControle << "PC: " << contPC << " | IR: " << instr << " " <<  valor1 << " " <<  valor2 << " " <<  valor3 << endl;
-        contPC++;
+        unicadeControle << "PC: " << PC << " | IR: " << instr << " " <<  valor1 << " " <<  valor2 << " " <<  valor3 << endl;
     } else {
         cout << "Erro ao abrir o arquivo de entrada" << endl;
     }
@@ -207,8 +385,26 @@ void executar(string instr, string valor1, string valor2, string valor3)
     {
         moveRegis(valor1, valor2);
         cout << "MOVE" << endl;
-    }    
+    }
+    if (instr == "ADD")
+    {
+        add(valor1, valor2, valor3);
+    }
+    if (instr == "SUB")
+    {
+        sub(valor1, valor2, valor3);
+    }
+    if (instr == "OR")
+    {
+        orReg(valor1, valor2, valor3);
+    }
+    if (instr == "AND")
+    {
+        andReg(valor1, valor2, valor3);
+    }
 }
+
+
 
 void arqOpen() {
     ifstream arqEntrada("entrada.txt");
@@ -226,36 +422,59 @@ void arqOpen() {
     }
 
     int PC = 0;
-    bool operando = true;
-    while ( operando && PC < (int)programa.size())
+    while (PC < (int)programa.size())
     {
         istringstream text(programa[PC]);
-        string instr = "", valor1, valor2, valor3;
+        string instr , valor1, valor2, valor3;
         text >> instr >> valor1 >> valor2 >> valor3;
-        
+        unicadeC(PC, instr, valor1, valor2, valor3);
+
         if (instr == "HALT")
         {
-            unicadeC(instr, valor1, valor2, valor3);
-            operando = false;
             cout << "HALT" << endl;
             break;
         }
-        if (instr == "BRANCH")
+        else if (instr == "BRANCH")
         {   
-            contPC = stoi(valor1);
-            unicadeC(instr, valor1, valor2, valor3);
             PC = stoi(valor1);
             cout << "BRANCH" << endl;
-        } 
-        if (instr != "HALT" && instr != "BRANCH" && operando != false)
+            continue;
+        }
+        else if (instr == "BNEG") 
         {
-            unicadeC(instr, valor1, valor2, valor3);
+            if (ultimoValor < 0) {
+                PC = stoi(valor1);
+                cout << "BNEG" << endl;
+                continue;
+            }
+            else
+            {
+                cout << "BNEG" << endl;
+                PC++;
+            }
+            continue;
+        }
+        else if (instr == "BZERO") 
+        {
+            if (ultimoValor == 0) {
+                PC = stoi(valor1);
+                cout << "BZERO" << endl;
+                continue;
+            }
+            else
+            {
+                PC++;
+                cout << "BZERO" << endl;
+            }
+            continue;
+        }
+        else
+        {
             executar(instr, valor1, valor2, valor3);
             PC++;
         }
     }
 }
-
 
 void iniciarReg()
 {
